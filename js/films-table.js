b08534324cf4df016cd7bfafc57a9021e2cea92d
imagineapril <julia.timokhina604@gmail.com>
2021-06-03 16:54:50 +0300

    ТЗ-16 Вывод фильмов в таблицу

--- js/films-table.js
+++ js/films-table.js
@@ -1,7 +1,13 @@
 const films = [
     {
+        start: '01:00',
+        title: 'XXL',
+        adult: true,
+        genre: []
+    },{
         start: '10:00',
         title: 'Человек-паук',
+        adult: false,
         genre: [
             {
                 name: 'Фантастика',
@@ -14,6 +20,11 @@ const films = [
             },
         ]
     }, {
+        start: '02:00',
+        title: 'XXXL',
+        adult: true,
+        genre: []
+    },{
         start: '12:00',
         title: 'Собачья жизнь 2',
         genre: [
@@ -58,22 +69,51 @@ const films = [
     }
 ];
 
-const tableBody = document.getElementById('block2-table-body');
-tableBody.innerHTML = ``;
+const filmHelper = {
+    getID() {
+        return this.id || this.title.replaceAll(' ', '-');
+    },
+    getTitle()  {
+        return this.title;
+    },
+    getStart()  {
+        return this.start;
+    },
+    getGenre()  {
+        return this.genre
+            .map(g => g.name)
+            .join(', ');
+    }
+}
 
-for (let index = 0; index < films.length; index++) {
-    tableBody.innerHTML += `
-<tr>
-                    <td class="grey"><svg width="12" height="12" viewBox="0 0 12 12" fill="none"
+function renderFilmTableItem(film){
+
+    return `
+    <tr>
+        <td>
+            <input type="checkbox" id="${filmHelper.getID.apply(film)}">
+            <label for="${filmHelper.getID.apply(film)}">
+                <svg width="12" height="12" viewBox="0 0 12 12" fill="none"
                             xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd" clip-rule="evenodd"
                                 d="M4.60581 8.79378L1.46056 5.93033L0.787354 6.66979L4.70255 10.2342L10.8223 2.94099L10.0562 2.2982L4.60581 8.79378Z"
                                 fill="white" />
-                        </svg>
-                    </td>
-                    <td>${films[index].start}</td>
-                    <td>${films[index].title}</td>
-                    <td>${films[index].genre.map(g => g.name)}</td>
-                </tr>
-`;
+                </svg> 
+            </label>
+        </td>
+        <td>${filmHelper.getStart.apply(film)}</td>
+        <td>${filmHelper.getTitle.apply(film)}</td>
+        <td>${filmHelper.getGenre.apply(film)}</td>
+    </tr>
+`
+
+}
+
+const tableBody = document.getElementById('block2-table-body');
+tableBody.innerHTML = ``;
+
+for (let index = 0; index < films.length; index++) {
+    if (!films[index].adult) {
+    tableBody.innerHTML += renderFilmTableItem(films[index]);
+    }
 }
